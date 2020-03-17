c981d66c9a42c4861b16563d89fc22dcf2793f42
Miika Johansson <miika.johansson@outlook.com>
2020-03-17 22:46:53 +0200

    Add missing files from previous commit

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    PlayerController controller = other.GetComponent<PlayerController>();
+
+    if (controller != null)
+    {
+      Debug.Log("IM HIT!");
+      StartCoroutine(LoadSceneAsync());
+    }
+  }
+
+  IEnumerator LoadSceneAsync()
+  {
+    AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Level1");
+
+    while (!asyncLoad.isDone)
+    {
+      yield return null;
+    }
+  }
+}
