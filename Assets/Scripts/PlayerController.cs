74ab53882259062b3e48f80e9c26536f2b84847a
Miika Johansson <miika.johansson@outlook.com>
2020-03-18 22:08:10 +0200

    Change aiming to use mouse with custom cursor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,6 +28,7 @@ public class PlayerController : MonoBehaviour
 
   Animator animator;
   Vector2 lookDirection = new Vector2(1, 0);
+  Vector2 aimDirection = new Vector2(1, 0);
   // Start is called before the first frame update
   void Start()
   {
@@ -45,6 +46,10 @@ public class PlayerController : MonoBehaviour
     float horizontal = Input.GetAxis("Horizontal");
     float vertical = Input.GetAxis("Vertical");
 
+    Vector2 crosshairInWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    aimDirection = crosshairInWorldPos - rigidbody2d.position;
+    aimDirection.Normalize();
+
     Vector2 move = new Vector2(horizontal, vertical);
 
     if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
@@ -53,17 +58,13 @@ public class PlayerController : MonoBehaviour
       lookDirection.Normalize();
     }
 
-
     animator.SetFloat("Look X", lookDirection.x);
     animator.SetFloat("Look Y", lookDirection.y);
     animator.SetFloat("Speed", move.magnitude);
 
     Vector2 position = rigidbody2d.position;
-
     position = position + move * moveSpeed * Time.deltaTime;
-
     rigidbody2d.MovePosition(position);
-
 
     if (isInvincible)
     {
@@ -86,7 +87,7 @@ public class PlayerController : MonoBehaviour
       }
     }
 
-    if (Input.GetKeyDown(KeyCode.C))
+    if (Input.GetButtonDown("Fire1"))
     {
       Launch();
     }
@@ -137,7 +138,7 @@ public class PlayerController : MonoBehaviour
     GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
     Projectile projectile = projectileObject.GetComponent<Projectile>();
-    projectile.Launch(lookDirection, 300);
+    projectile.Launch(aimDirection, 300);
 
     animator.SetTrigger("Launch");
 
---
+++ Assets/Scripts/UICursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICursor : MonoBehaviour
+{
+  public static UICursor instance { get; private set; }
+  public Texture2D cursorTexture;
+  void Awake()
+  {
+    instance = this;
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+  }
+
+}
