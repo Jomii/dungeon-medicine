96dcbc1ce0780568604c5be778d40964ce15aa2b
Miika Johansson <miika.johansson@outlook.com>
2020-03-19 17:07:32 +0200

    Start making ranged enemy type

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,10 +6,18 @@ public class EnemyController : MonoBehaviour
 {
   public float speed = 3.0f;
   public ParticleSystem smokeEffect;
+  // Ranged behaviour
+  public bool ranged = false;
+  public GameObject projectilePrefab;
+  public Transform[] moveSpots;
+  public float moveWaitTime;
 
   Rigidbody2D rigidbody2d;
   Transform target;
   bool broken = true;
+  // Ranged behaviour
+  int randomSpot;
+  float waitTime;
 
   Animator animator;
   // Start is called before the first frame update
@@ -18,6 +26,12 @@ public class EnemyController : MonoBehaviour
     rigidbody2d = GetComponent<Rigidbody2D>();
     target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     animator = GetComponent<Animator>();
+
+    if (ranged)
+    {
+      waitTime = moveWaitTime;
+      randomSpot = Random.Range(0, moveSpots.Length);
+    }
   }
 
   // Update is called once per frame
@@ -28,7 +42,18 @@ public class EnemyController : MonoBehaviour
       return;
     }
 
-    // Debug.Log("Target position: " + target.position);
+    if (!ranged)
+    {
+      MeleeBehaviour();
+    }
+    else
+    {
+      RangedBehaviour();
+    }
+  }
+
+  void MeleeBehaviour()
+  {
     Vector2 position = rigidbody2d.position;
     Vector2 directionToTarget = new Vector2(target.position.x - position.x, target.position.y - position.y);
 
@@ -43,6 +68,39 @@ public class EnemyController : MonoBehaviour
     }
   }
 
+  void RangedBehaviour()
+  {
+    Vector2 position = rigidbody2d.position;
+    Vector2 directionToTarget = new Vector2(moveSpots[randomSpot].position.x - position.x, moveSpots[randomSpot].position.y - position.y);
+
+    if (Vector2.Distance(position, moveSpots[randomSpot].position) > 0.2f)
+    {
+      directionToTarget.Normalize();
+      position = position + directionToTarget * speed * Time.deltaTime;
+      rigidbody2d.MovePosition(position);
+
+      animator.SetFloat("Move X", directionToTarget.x);
+      animator.SetFloat("Move Y", directionToTarget.y);
+    }
+
+    if (!Mathf.Approximately(directionToTarget.x, 0.2f) || !Mathf.Approximately(directionToTarget.y, 0.2f))
+    {
+      if (waitTime <= 0)
+      {
+        randomSpot = Random.Range(0, moveSpots.Length);
+        waitTime = moveWaitTime;
+      }
+      else
+      {
+        waitTime -= Time.deltaTime;
+      }
+    }
+
+    // if (Vector2.Distance(position, target.position) < 5.0f) {
+    //   Shoot(directionToTarget);
+    // }
+  }
+
   void OnCollisionEnter2D(Collision2D other)
   {
     PlayerController player = other.gameObject.GetComponent<PlayerController>();
@@ -52,6 +110,13 @@ public class EnemyController : MonoBehaviour
       player.ChangeHealth(-1);
     }
   }
+
+  // void Shoot(Vector2 directionToTarget) {
+  //   GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+  //   Projectile projectile = projectileObject.GetComponent<Projectile>();
+  //   projectile.Launch(directionToTarget, 300);
+  // }
 
   public void Fix()
   {
