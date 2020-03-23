c32b63290a2ba6bb56505e084bab6bb9dccddb19
Miika Johansson <miika.johansson@outlook.com>
2020-03-23 19:13:26 +0200

    Add IntroScene with transition to next scene

---
+++ Assets/Scripts/IntroTransition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroTransition : MonoBehaviour
+{
+  public float displayContinue = 10.0f;
+  public GameObject continueText;
+  // Start is called before the first frame update
+  void Start()
+  {
+    continueText.SetActive(false);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (displayContinue >= 0)
+    {
+      displayContinue -= Time.deltaTime;
+    }
+    else
+    {
+      continueText.SetActive(true);
+
+      if (Input.GetButtonDown("Jump"))
+      {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+      }
+    }
+  }
+}
