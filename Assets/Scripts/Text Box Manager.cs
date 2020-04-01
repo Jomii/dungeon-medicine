525359be8db9ec782f4c95ba664d2aace6a016b9
Miika Johansson <miika.johansson@tuni.fi>
2020-04-01 17:17:51 +0300

    Fix errors caused by TextBoxManager

--- Assets/Scripts/Text Box Manager.cs
+++ Assets/Scripts/Text Box Manager.cs
@@ -1,53 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class TextBoxManager : MonoBehaviour
 {
-    public GameObject textBox;
+  public GameObject textBox;
 
-    public Text theText;
+  public Text theText;
 
-    public TextAsset textFile;
-    public string[] textLines;
+  public TextAsset textFile;
+  public string[] textLines;
 
-    public int currentLine;
-    public int endAtLine;
+  public int currentLine;
+  public int endAtLine;
 
-    public PlayerController player;
+  public PlayerController player;
 
-    // Use this for initialization
-    void Start()
+  // Use this for initialization
+  void Start()
+  {
+    player = FindObjectOfType<PlayerController>();
+
+    if (textFile != null)
     {
-        player = FindObjectOfType<PlayerController>();
+      textLines = (textFile.text.Split('\n'));
 
-        if(textFile != null)
-        {
-            textLines = (textFile.text.Split('\n'));
-
-        }
-
-        if(endAtLine == 0)
-        {
-            endAtLine = textLines.Length - 1;
-
-        }
-        
     }
 
-    void Update()
+    if (endAtLine == 0)
     {
+      endAtLine = textLines.Length - 1;
 
-        theText.text = textLines[currentLine];
-
-        if(Input.GetKeyDown(KeyCode.Retrun))
-        {
-            currentLine +=1; 
-        }
-
-        if(currentLine > endAtLine)
-        {
-            textBox.SetActive(false);
-        }
     }
+
+  }
+
+  void Update()
+  {
+
+    theText.text = textLines[currentLine];
+
+    if (Input.GetKeyDown(KeyCode.Return))
+    {
+      currentLine += 1;
+    }
+
+    if (currentLine > endAtLine)
+    {
+      textBox.SetActive(false);
+    }
+  }
 }
