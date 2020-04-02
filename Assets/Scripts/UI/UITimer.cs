c45cc92e11da6dd07590e9bfa454267b4aa21629
Miika Johansson <miika.johansson@tuni.fi>
2020-04-02 10:53:59 +0300

    Add level timer to UI

---
+++ Assets/Scripts/UI/UITimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UITimer : MonoBehaviour
+{
+  public static UITimer instance { get; private set; }
+  TextMeshProUGUI timerText;
+  public int LevelTime = 60;
+  int timer;
+
+  void Awake()
+  {
+    timerText = gameObject.GetComponent<TextMeshProUGUI>();
+    instance = this;
+  }
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    timer = LevelTime;
+    InvokeRepeating("DrawTimer", 0.0f, 1.0f);
+  }
+
+  void DrawTimer()
+  {
+    timer--;
+
+    if (timer <= 0)
+    {
+      CancelInvoke(); // Stop InvokeRepeating calls
+      timerText.color = Color.red;
+    }
+
+    timerText.text = timer.ToString();
+  }
+}
