0c8ef4995e1edb4befe65f7ada3293ef821199af
Miika Johansson <miika.johansson@tuni.fi>
2020-04-02 11:35:32 +0300

    Enable changing volume from main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
+  public AudioMixer audioMixer;
   public void PlayGame()
   {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  }
+
+  public void SetVolume(float volume)
+  {
+    // Debug.Log(volume);
+    audioMixer.SetFloat("Volume", volume);
   }
 
   public void QuitGame()
